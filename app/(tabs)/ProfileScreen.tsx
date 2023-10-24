60f15d5d1a473755107d6ae5a4648f562a9bdf18
Nevil Jobanputra <nevil.j@ahduni.edu.in>
2023-10-24 23:10:02 +0530

    Profile Screen

--- app/(tabs)/ProfileScreen.tsx
+++ app/(tabs)/ProfileScreen.tsx
@@ -1,29 +1,65 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import MasonryList from '../../components/MasonryList';
+import pins from '../../assets/data/pins';
+import { Feather, Entypo } from '@expo/vector-icons';
 
 export default function ProfileScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+
+        <View style={styles.icons}>
+          <Feather name='share' size={24} color="black" style={styles.icons} />
+          <Entypo name='dots-three-vertical' size={24} color="black" style={styles.icons} />
+        </View>
+      <Image 
+      source={{
+        uri:"https://avatars.githubusercontent.com/u/56132780?v=4"
+      }}
+      style={styles.image}
+      />
+      <Text style={styles.title}>Nevil Jobanputra</Text>
+      <Text style={styles.subtitle}>123 Followers | 534 Followings</Text>
+
+      </View>
+      
+      <MasonryList pins={pins} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width:"100%",
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    margin:10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  subtitle:{
+    color:"#181818",
+    fontWeight:"600",
+    margin:10,
   },
+  header:{
+    alignItems:"center",
+  },
+  image:{
+    width:200,
+    aspectRatio:1,
+    borderRadius:200,
+    marginVertical: 10,
+  },
+  icons:{
+    flexDirection:"row",
+    alignSelf:"flex-end",
+    padding:10,
+  },
+  icon:{
+    paddingHorizontal:10,
+  }
 });
